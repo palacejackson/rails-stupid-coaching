e1f30c9fa813c216079768276b38479fdd0ac923
Alice Jackson <alicejj92@gmail.com>
2023-05-22 14:36:06 +0100

    headless chrome tested

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,9 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = if params[:answer].include?('?')
+    @answer = if params[:question].include?('?')
                 'Silly question, get dressed and go to work!'
-              elsif params[:answer] == 'I am going to work'
+              elsif params[:question] == 'I am going to work'
                 'Great!'
               else
                 'I don\'t care, get dressed and go to work!'
